49fcd2e0d8b5e4427a68c83edf20be4bf1096735
CodewithRajDeep <deepraj6897@gmail.com>
2024-10-10 16:33:28 +0530

    final deploy

--- backend/server.js
+++ backend/server.js
@@ -8,27 +8,39 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
-import { app, server } from "./socket/socket.js";
+import { app, server } from "./socket/socket.js"; 
 
 dotenv.config();
 
 const __dirname = path.resolve();
 const PORT = process.env.PORT || 5000;
 
+
 app.use(express.json());
 app.use(cookieParser());
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
+
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
-server.listen(PORT, () => {
-	connectToMongoDB();
-	console.log(`Server Running on port ${PORT}`);
-});
+
+connectToMongoDB();
+
+
+export default app;
+
+
+if (process.env.NODE_ENV !== "production") {
+    server.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
