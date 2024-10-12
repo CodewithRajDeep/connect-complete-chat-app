494d4bf79c9593e290ab8cd29ae78f47262aa05e
CodewithRajDeep <deepraj6897@gmail.com>
2024-10-12 20:59:54 +0530

    modified file structure

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import cors from 'cors';
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
@@ -19,7 +19,6 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 
-const cors = require('cors');
 app.use(cors({ origin: 'https://connect-chat-app-eight.vercel.app' }));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
